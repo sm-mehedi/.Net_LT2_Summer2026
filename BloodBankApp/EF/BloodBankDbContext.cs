0b2188d94c06d83cdc8a011e35b8e6b3b339f8a2
sm-mehedi <hasanmehedi63395@gmail.com>
2026-07-24 12:27:13 +0600

    feat: implement EF Core database context and entity mappings

---
+++ BloodBankApp/EF/BloodBankDbContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using BloodBankApp.EF.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloodBankApp.EF;
+
+public partial class BloodBankDbContext : DbContext
+{
+    public BloodBankDbContext(DbContextOptions<BloodBankDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Donation> Donations { get; set; }
+
+    public virtual DbSet<Donor> Donors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Donation>(entity =>
+        {
+            entity.HasKey(e => e.DonationId).HasName("PK__Donation__C5082EFB3A33511B");
+
+            entity.ToTable("Donation");
+
+            entity.Property(e => e.CampName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.Donor).WithMany(p => p.Donations)
+                .HasForeignKey(d => d.DonorId)
+                .HasConstraintName("FK_Donation_Donor");
+        });
+
+        modelBuilder.Entity<Donor>(entity =>
+        {
+            entity.HasKey(e => e.DonorId).HasName("PK__Donor__052E3F780DC4ADDD");
+
+            entity.ToTable("Donor");
+
+            entity.Property(e => e.BloodGroup)
+                .HasMaxLength(5)
+                .IsUnicode(false);
+            entity.Property(e => e.City)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.ContactNo)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.FullName)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
