94f6ebd3ba45702b2fa461eae0bb62378dc8f618
sm-mehedi <hasanmehedi63395@gmail.com>
2026-07-24 12:23:23 +0600

    feat: add create, read, update, and delete functionality for donors

--- BloodBankApp/obj/Debug/net8.0/BloodBankApp.AssemblyInfo.cs
+++ BloodBankApp/obj/Debug/net8.0/BloodBankApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodBankApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95088eca70af95a28471d898eb067b7701b2de70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90e2696b75ed38bb124c03a9577eee8c3818ffaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodBankApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodBankApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
